0521b84eb7012285d424daff12fc5b9d1189d551
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-10-17 20:48:06 -0400

    moving code into factory functions
    Ive been taking my time to wrap my head around understanding factory functions. I have been working to combine my previous function with some from scratch factory functionality to create this game with as little amount of functions as possible. I still have a ways to go.

--- main.js
+++ main.js
@@ -1,52 +1,64 @@
-let gridboxContainer = document.createElement('div');
-gridboxContainer.id = 'gridbox-container';
-document.body.appendChild(gridboxContainer);
-
-let gridbox = []
 let whosTurn = '';
+const winningCombinations = [
+    [0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+let winner = null;
 
 const gameBoard = (() => {
-    let gameBoardGrid = ['', '', '', '', '', '', '', '', ''];
     let CoinFlip = () => {
         let rng = Math.floor(Math.random() * 2);
-        console.log(rng); //will take this out once complete
         if(rng === 0){
             return whosTurn = 'O';
         }else if(rng === 1){
             return whosTurn = 'X';
-        }
+    }};
+    const allBoxes = document.querySelectorAll('.box');
+    let handleTurn = () => {
+        allBoxes.forEach(box => {
+            box.addEventListener('click', () => {
+                event.target.innerHTML = whosTurn;
+                if(whosTurn === 'X'){
+                    event.target.innerHTML = 'X';
+                    whosTurn = 'O';
+                } else if(whosTurn === 'O'){
+                    event.target.innerHTML = 'O';
+                    whosTurn = 'X';
+                }
+                winningCombos.forEach(() => {
+                   
+                });
+        });
     }
+
     return {
-        gameBoardGrid,
         CoinFlip,
+        allBoxes,
+        handleTurn
     };
 })();
 
+const gameStart = () => {
 gameBoard.CoinFlip()
-
-console.log(whosTurn) //will take this out once complete
-
-for (let i = 0; i < gameBoard.gameBoardGrid.length; i++){
-    gridbox = gridbox['gridbox_'+i]
-    gridbox = document.createElement('div');
-    gridbox.id = 'gridbox_'+i;
-    gridbox.className = 'gridboxes';
-    document.getElementById('gridbox-container').appendChild(gridbox);
-    gridbox.innerHTML = gameBoard.gameBoardGrid[i];
-    gridbox.addEventListener('click', () => {
-        if(whosTurn === 'X'){
-            event.target.innerHTML = 'X';
-            return whosTurn = 'O';
-        } else if(whosTurn === 'O'){
-            event.target.innerHTML = 'O';
-            return whosTurn = 'X';
-        } else return console.log('error deciding whos turn it is');
-    });
+gameBoard.handleTurn()
+console.log("player" + ' "' + whosTurn + '" ' + "will go first.")
 }
 
-// if(gameBoard.gameBoardGrid[0, 1, 2] || gameBoard.gameBoardGrid[ 0, 4 ,8] || gameBoard.gameBoardGrid[0, 3, 6]
-//     || gameBoard.gameBoardGrid[1, 4, 7] || gameBoard.gameBoardGrid[2, 4, 6] || gameBoard.gameBoardGrid[2, 5, 8]
-//     || gameBoard.gameBoardGrid[3, 4, 5] || gameBoard.gameBoardGrid[6, 7 ,8] === gameBoard.gameBoardGrid['X', 'X', 'X'] || gameBoardGrid['O', 'O', 'O'])
-//     { 
-//         console.log('winner winner');
-// };
+gameStart()
+
+
+
+// let player = () => {
+//     let X = () => {
+
+//     }
+//     let O = () => {
+
+//     }
+// }
