d9f8b896a45ffe6f34f20f4498d1c3201d062436
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-10-23 14:14:29 -0400

    finished eventListeners, factory functions and findWinner function
    This was a huge update. I have been working on this for many nights now trying to get all the code to function properly. I was getting tripped up and trying to rework code that wasn't my main issue. The main issue I was having in the initial tries of this project is that I was using the variable gameBoard to create all the divs in a for loop but was not actually changing any of the objects in the array but instead my event listeners were changing the innerHTML of the divs. After figuring that out, It took me a while to reorganize my code to work together again and on top of that trying to compile my code into factory functions. 
    
    The project functionality is finally complete.

--- main.js
+++ main.js
@@ -1,64 +1,105 @@
 let whosTurn = '';
-const winningCombinations = [
-    [0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-]
-let winner = null;
+const resultsDiv = document.getElementById('results');
+const allBoxes = document.querySelectorAll('.box')
 
-const gameBoard = (() => {
+const board = (() => {
+    let gameBoard = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+    const createBoard = () => {
+        for (let i = 0; i < gameBoard.length; i++) {
+            gridbox = document.createElement('div');
+            gridbox.id = i;
+            gridbox.className = 'box';
+            document.getElementById('gridbox-container').appendChild(gridbox);
+            gridbox.innerHTML = gameBoard[i];
+            gridbox.addEventListener('click', player.turnHandler);
+        }
+    }
+    const endGame = () => {
+            allBoxes.forEach(box => {
+            box.removeEventListener('click', turnHandler);
+        });
+    }
+    return {
+        gameBoard,
+        createBoard,
+        endGame,
+    };
+})();
+
+const player = (() => {
     let CoinFlip = () => {
         let rng = Math.floor(Math.random() * 2);
         if(rng === 0){
             return whosTurn = 'O';
         }else if(rng === 1){
             return whosTurn = 'X';
-    }};
-    const allBoxes = document.querySelectorAll('.box');
-    let handleTurn = () => {
-        allBoxes.forEach(box => {
-            box.addEventListener('click', () => {
-                event.target.innerHTML = whosTurn;
-                if(whosTurn === 'X'){
-                    event.target.innerHTML = 'X';
-                    whosTurn = 'O';
-                } else if(whosTurn === 'O'){
-                    event.target.innerHTML = 'O';
-                    whosTurn = 'X';
-                }
-                winningCombos.forEach(() => {
-                   
-                });
-        });
-    }
-
-    return {
-        CoinFlip,
-        allBoxes,
-        handleTurn
+        }
     };
+    let turnHandler = (e) => {
+        let cell = e.target;
+        cell.removeEventListener('click', turnHandler);
+        console.log(cell)
+        if(whosTurn === 'X'){
+            board.gameBoard.splice(cell.id, 1, 'X');
+            cell.innerHTML = board.gameBoard[cell.id];
+            findWinner();
+            whosTurn = 'O';
+        } else if(whosTurn === 'O'){
+            board.gameBoard.splice(cell.id, 1, 'O');
+            cell.innerHTML = board.gameBoard[cell.id];
+            findWinner();
+            whosTurn = 'X';
+        }
+        cell.className = 'player' + whosTurn;
+    }
+    let findWinner = () => {
+        if (board.gameBoard[0] === whosTurn) {
+            if (board.gameBoard[1] === whosTurn && board.gameBoard[2] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins across the top`;
+            }
+            if (board.gameBoard[3] === whosTurn && board.gameBoard[6] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins down the left`;
+            }
+            if (board.gameBoard[4] === whosTurn && board.gameBoard[8] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins diagonally from top right to bottom left`;
+            }
+        }
+        if (board.gameBoard[8] === whosTurn) {
+            if (board.gameBoard[2] === whosTurn && board.gameBoard[5] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins down the right`;
+            }
+            if (board.gameBoard[6] === whosTurn && board.gameBoard[7] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins across the bottom`;
+            }
+        }
+        if (board.gameBoard[4] === whosTurn) {
+            if (board.gameBoard[1] === whosTurn && board.gameBoard[7] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins vertically in the middle`;
+            }
+            if (board.gameBoard[3] === whosTurn && board.gameBoard[5] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins horizontally in the middle`;
+            }
+            if (board.gameBoard[2] === whosTurn && board.gameBoard[6] === whosTurn) {
+                resultsDiv.innerHTML = `player ${whosTurn} wins diagonally from top left to bottom right `;
+            }
+        }
+    }
+    return {
+        whosTurn, 
+        CoinFlip,
+        turnHandler,
+        findWinner,
+    }
 })();
 
 const gameStart = () => {
-gameBoard.CoinFlip()
-gameBoard.handleTurn()
-console.log("player" + ' "' + whosTurn + '" ' + "will go first.")
+    board.createBoard()
+    player.CoinFlip()
+    resultsDiv.innerHTML = "player" + ' "' + whosTurn + '" ' + "will go first.";
 }
 
-gameStart()
-
-
-
-// let player = () => {
-//     let X = () => {
-
-//     }
-//     let O = () => {
-
-//     }
-// }
+gameStart();
